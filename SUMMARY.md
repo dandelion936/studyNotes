2fba0d28c10129dbdb61ea325ed5f56fcde96dc1
dandelion936 <1260481758@qq.com>
2017-08-08 01:03:29 +0800

    dav 的 react 示例

---
+++ SUMMARY.md
@@ -0,0 +1,10 @@
+# 目录
+
+* [简介](README.md)  
+* [框架篇](frame/chapter.md)  
+    * [AntDesign](frame/AntDesign)
+* [javaScript篇](JavaScript/chapter.md)  
+* [PHP篇](PHP/chapter.md)  
+* [React篇](react/chapter.md)
+
+
---
+++ react/实战练习/dva-quickstart/src/components/ProductList.js
@@ -0,0 +1,33 @@
+import React, { PropTypes } from 'react';
+import { Table, Popconfirm, Button } from 'antd';
+
+const ProductList = ({ onDelete, products }) => {
+  const columns = [{
+    title: 'Name',
+    dataIndex: 'name',
+  }, {
+    title: 'Action',
+
+
+    render: (text, record) => {
+      return (
+        <Popconfirm title="Delete?" onConfirm={() => onDelete(record.id)}>
+          <Button>Delete</Button>
+        </Popconfirm>
+      );
+    },
+  }];
+  return (
+    <Table
+      dataSource={products}
+      columns={columns}
+    />
+  );
+};
+
+ProductList.propTypes = {
+  onDelete: PropTypes.func.isRequired,
+  products: PropTypes.array.isRequired,
+};
+
+export default ProductList;
